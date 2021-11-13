9d096b14c07f78dd1b0c3a28127ac4683547ffc6
seonghooony <sh-k@kakao.com>
2021-11-13 11:52:20 +0900

    21-11-13 study

--- grammar/08. optional/optional.playground/Contents.swift
+++ grammar/08. optional/optional.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 var name: String?
 //var name: String
 
-var optionalName: String? = "Gunter"
+var optionalName: String? = nil
 //var requiredName: String = optionalName
 
 print(optionalName)
---
+++ grammar/16. type casting/type casting.playground/Contents.swift
@@ -0,0 +1,63 @@
+import UIKit
+import Foundation
+import Darwin
+
+
+//is, as
+
+class MediaItem {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name:  name)
+    }
+}
+
+class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+//타입추론에 의해 MediaItem 클래스라는걸 예측함
+let library = [
+    Movie(name: "기생충", director: "봉준호"),
+    Song(name: "Butter", artist: "BTS"),
+    Movie(name: "올드보이", director: "박찬욱"),
+    Song(name: "wonderwall", artist: "Oasis"),
+    Song(name: "Rain", artist: "이적")
+]
+
+
+var movieCount = 0
+var songCount = 0
+
+for item in library {
+    if item is Movie {
+        movieCount += 1
+        print(item.name)
+    }else if item is Song {
+        songCount += 1
+    }
+}
+
+print("Media library contains \(movieCount) movies and \(songCount) songs")
+
+for item in library {
+    //item은 MediaItem형태이므로 down casting을 통해 Movie나 Song형태로 변환시킴
+    if let movie = item as? Movie {
+        print("movie: \(movie.name), dir. \(movie.director)")
+    } else if let song = item as? Song {
+        print("Song: \(song.name), by \(song.artist)")
+    }
+    
+}

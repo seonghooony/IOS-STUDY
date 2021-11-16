53aae269c792f031f2d1a6d393a6f4a340c52387
seonghooony <sh-k@kakao.com>
2021-11-16 14:09:18 +0900

    21-11-16 exaple project git commit

---
+++ 02. basic project/01. ExampleProject/ExampleProject/ExampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by SeongHoon Kim on 2021/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Do something
+    }
+
+
+}
+

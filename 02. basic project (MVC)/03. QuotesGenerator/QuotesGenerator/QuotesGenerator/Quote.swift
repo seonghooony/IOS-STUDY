2d0027138b807bd2db1484edbfbe8fa29f64f08a
seonghooony <sh-k@kakao.com>
2021-11-18 16:35:07 +0900

    21-11-18 study

---
+++ 02. basic project (MVC)/03. QuotesGenerator/QuotesGenerator/QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by SeongHoon Kim on 2021/11/18.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}

7f30a8d6dad3d37a9416d0d3504341310672ffb0
seonghooony <sh-k@kakao.com>
2021-12-10 15:46:00 +0900

    21-12-10 study

---
+++ 03. intermediate project/01. FirebaseLoginSampleApp/FirebaseLoginSampleApp/FirebaseLoginSampleApp/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  FirebaseLoginSampleApp
+//
+//  Created by SeongHoon Kim on 2021/12/09.
+//
+
+import UIKit
+import FirebaseAuth
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+        
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+        환영합니다.
+        \(email)님
+        """
+    }
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+}

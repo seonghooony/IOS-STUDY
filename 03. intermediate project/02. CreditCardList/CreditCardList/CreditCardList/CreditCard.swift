6722be21cf780517f9d264b96d2175360979a4c5
seonghooony <sh-k@kakao.com>
2021-12-13 21:33:07 +0900

    21-12-13 study

---
+++ 03. intermediate project/02. CreditCardList/CreditCardList/CreditCardList/CreditCard.swift
@@ -0,0 +1,28 @@
+//
+//  CreditCard.swift
+//  CreditCardList
+//
+//  Created by SeongHoon Kim on 2021/12/13.
+//
+
+import Foundation
+
+struct CreditCard: Codable {
+    let id: Int
+    let rank: Int
+    let name: String
+    let cardImageURL: String
+    let promotionDetail: PromotionDetail
+    let isSelected: Bool?
+    
+}
+
+
+struct PromotionDetail: Codable {
+    let companyName: String
+    let period: String
+    let amount: String
+    let benefitContdition: String
+    let benefitDetail: String
+    let benefitDate: String
+}

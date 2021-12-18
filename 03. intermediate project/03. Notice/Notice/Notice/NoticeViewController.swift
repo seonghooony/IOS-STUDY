fcc667404ac03192d472dbe1fa995a1b39c1f166
seonghooony <sh-k@kakao.com>
2021-12-18 16:34:36 +0900

    21-12-18 study

---
+++ 03. intermediate project/03. Notice/Notice/Notice/NoticeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  NoticeViewController.swift
+//  Notice
+//
+//  Created by SeongHoon Kim on 2021/12/18.
+//
+
+import UIKit
+
+class NoticeViewController: UIViewController {
+
+    var noticeContents: (title:String, detail: String, date: String)?
+    
+    @IBOutlet weak var noticeView: UIView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        noticeView.layer.cornerRadius = 6
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        
+        guard let noticeContents = noticeContents else { return }
+        titleLabel.text = noticeContents.title
+        detailLabel.text = noticeContents.detail
+        dateLabel.text = noticeContents.date
+        
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+}

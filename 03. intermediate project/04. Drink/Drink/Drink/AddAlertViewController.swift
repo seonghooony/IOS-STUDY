a1ac74ad2500eb9a08b5f9489b94a5a954b1edcd
seonghooony <sh-k@kakao.com>
2021-12-22 17:33:16 +0900

    21-12-22 study

---
+++ 03. intermediate project/04. Drink/Drink/Drink/AddAlertViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddAlertViewController.swift
+//  Drink
+//
+//  Created by SeongHoon Kim on 2021/12/22.
+//
+
+import UIKit
+
+
+class AddAlertViewController: UIViewController {
+    var pickedDate: ((_ date: Date) -> Void)?
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func dismissButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        pickedDate?(datePicker.date)
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ 03. intermediate project/04. Drink/Drink/Drink/Alert.swift
@@ -0,0 +1,27 @@
+//
+//  Alert.swift
+//  Drink
+//
+//  Created by SeongHoon Kim on 2021/12/22.
+//
+
+import Foundation
+
+struct Alert: Codable {
+    var id: String = UUID().uuidString
+    let date: Date
+    var isOn: Bool
+    
+    var time: String {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "hh:mm"
+        return timeFormatter.string(from: date)
+    }
+    
+    var meridiem: String {
+        let meridiemFormatter = DateFormatter()
+        meridiemFormatter.dateFormat = "a"
+        meridiemFormatter.locale = Locale(identifier: "ko")
+        return meridiemFormatter.string(from: date)
+    }
+}

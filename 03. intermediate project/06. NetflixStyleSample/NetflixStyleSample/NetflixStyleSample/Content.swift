eb736a4fd8adec7b8c684370d252b1261e3a807b
seonghooony <sh-k@kakao.com>
2022-01-03 11:52:14 +0900

    22-1-3 study

---
+++ 03. intermediate project/06. NetflixStyleSample/NetflixStyleSample/NetflixStyleSample/Content.swift
@@ -0,0 +1,33 @@
+//
+//  Content.swift
+//  NetflixStyleSample
+//
+//  Created by SeongHoon Kim on 2022/01/02.
+//
+
+import UIKit
+
+struct Content: Decodable {
+    let sectionType: SectionType
+    let sectionName: String
+    let contentItem: [Item]
+    
+    enum SectionType: String, Decodable {
+        case basic
+        case main
+        case large
+        case rank
+    }
+}
+
+
+struct Item: Decodable {
+    let description: String
+    let imageName: String
+    
+    var image: UIImage {
+        return UIImage(named: imageName) ?? UIImage()
+    }
+}
+
+
---
+++ 03. intermediate project/06. NetflixStyleSample/NetflixStyleSample/NetflixStyleSample/HomeViewController.swift
@@ -0,0 +1,87 @@
+//
+//  HomeViewController.swift
+//  NetflixStyleSample
+//
+//  Created by SeongHoon Kim on 2022/01/02.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    
+    var contents: [Content] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //네비게이션 설정
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        let image = UIImage(named: "netflix_icon")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+        
+        //data 설정, 가져오기
+        contents = getContents()
+        
+        //collectionView Item(Cell) 설정
+        collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
+        collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
+    }
+    
+    func getContents() -> [Content] {
+        guard let path = Bundle.main.path(forResource: "Content", ofType: "plist"),
+              let data = FileManager.default.contents(atPath: path),
+              let list = try? PropertyListDecoder().decode([Content].self, from: data) else { return []}
+        return list
+    }
+}
+
+//UICollectionView DataSource, Delegate
+extension HomeViewController {
+    //섹션당 보여질 셀의 개수
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            return contents[section].contentItem.count
+        }
+    }
+    
+    //콜렉션뷰 셀 설정
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch contents[indexPath.section].sectionType {
+        case .basic, .large:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewCell", for: indexPath) as? ContentCollectionViewCell else { return UICollectionViewCell() }
+            cell.imageView.image = contents[indexPath.section].contentItem[indexPath.row].image
+            return cell
+        default:
+            return UICollectionViewCell()
+        }
+    }
+    
+    //헤더뷰 설정
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ContentCollectionViewHeader", for: indexPath) as? ContentCollectionViewHeader else { fatalError("Could not dequeue Header") }
+            headerView.sectionNameLabel.text = contents[indexPath.section].sectionName
+            return headerView
+        } else {
+            return UICollectionReusableView()
+        }
+    }
+    
+    //섹션 개수 설정
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return contents.count
+    }
+    
+    //셀 선택
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let sectionName = contents[indexPath.section].sectionName
+        print("TEST: \(sectionName) 섹션의 \(indexPath.row+1)번째 콘텐츠")
+    }
+}

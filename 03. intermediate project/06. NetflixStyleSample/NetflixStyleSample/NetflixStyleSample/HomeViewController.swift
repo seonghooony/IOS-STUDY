aa89d4678adc5a45c60bfae8ebde1d8de00d50be
seonghooony <sh-k@kakao.com>
2022-01-04 17:55:55 +0900

    22-1-4 study

--- 03. intermediate project/06. NetflixStyleSample/NetflixStyleSample/NetflixStyleSample/HomeViewController.swift
+++ 03. intermediate project/06. NetflixStyleSample/NetflixStyleSample/NetflixStyleSample/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class HomeViewController: UICollectionViewController {
     
@@ -83,5 +84,25 @@ extension HomeViewController {
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let sectionName = contents[indexPath.section].sectionName
         print("TEST: \(sectionName) 섹션의 \(indexPath.row+1)번째 콘텐츠")
+    }
+}
+
+//SwiftUI를 활용한 미리보기
+struct HomeVuewController_Previews: PreviewProvider {
+    static var previews: some View {
+        Container().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            let layout = UICollectionViewLayout()
+            let homeViewController = HomeViewController(collectionViewLayout: layout)
+            return UINavigationController(rootViewController: homeViewController)
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+        
+        typealias UIViewControllerType = UIViewController
     }
 }

478a098e9ed7722453569f56d925b6779e5082c3
seonghooony <sh-k@kakao.com>
2022-01-12 13:57:01 +0900

    22-1-12 study

--- 03. intermediate project/07. brewery/brewery/brewery/Beer.swift
+++ 03. intermediate project/07. brewery/brewery/brewery/Beer.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Beer: Decodable {
     let id: Int?
     let name, taglineString, description, brewersTips, imageURL: String?
-    let foodParing: [String]?
+    let foodPairing: [String]?
     
     var tagLine: String {
         let tags = taglineString?.components(separatedBy: ". ")
@@ -29,7 +29,7 @@ struct Beer: Decodable {
         case taglineString = "tagline"
         case imageURL = "image_url"
         case brewersTips = "brewers_tips"
-        case foodParing = "food_paring"
+        case foodPairing = "food_pairing"
         
     }
 }
--- 03. intermediate project/07. brewery/brewery/brewery/BeerListViewController.swift
+++ 03. intermediate project/07. brewery/brewery/brewery/BeerListViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class BeerListViewController: UITableViewController {
     
     var beerList = [Beer]()
+    var dataTasks = [URLSessionTask]()
+    var currentPage = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,18 +25,24 @@ class BeerListViewController: UITableViewController {
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150
         
+        //paging
+        tableView.prefetchDataSource = self
+        
+        fetchBeer(of: currentPage)
         
     }
     
     
 }
 
-extension BeerListViewController {
+extension BeerListViewController: UITableViewDataSourcePrefetching {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return beerList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Rows: \(indexPath.row)")
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as! BeerListCell
 
         //guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
@@ -44,4 +52,79 @@ extension BeerListViewController {
         return cell
         
     }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedBeer = beerList[indexPath.row]
+        let detialViewController = BeerDetailViewController()
+        
+        detialViewController.beer = selectedBeer
+        self.show(detialViewController, sender: nil)
+    }
+    
+    
+    //paging
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard currentPage != 1 else { return }
+        indexPaths.forEach {
+            print("PrefatchRows: \($0.row)")
+            if ($0.row + 1)/25 + 1 == currentPage {
+                self.fetchBeer(of: currentPage)
+            }
+        }
+    }
 }
+
+//Data Fetching
+private extension BeerListViewController {
+    func fetchBeer(of page: Int) {
+        
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)"),
+              dataTasks.firstIndex(where: { $0.originalRequest?.url == url }) == nil
+        else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) {[weak self] data, response, error in
+            guard error == nil,
+                  let self = self,
+                  let response = response as? HTTPURLResponse,
+                  let data = data,
+                  let beers = try? JSONDecoder().decode([Beer].self, from: data)
+            else {
+                print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            switch response.statusCode {
+            case (200...299)://성공
+                self.beerList += beers
+                self.currentPage += 1
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case (400...499)://클라이언트 에러
+                print("""
+                    ERROR:Client ERROR \(response.statusCode)
+                    Response: \(response)
+                """)
+            case (500...599)://서버에러
+                print("""
+                    ERROR:Server ERROR \(response.statusCode)
+                    Response: \(response)
+                """)
+            default:
+                print("""
+                    ERROR \(response.statusCode)
+                    Response: \(response)
+                """)
+                
+            }
+        }
+        
+        dataTask.resume()
+        dataTasks.append(dataTask)
+    }
+}
+
+
+

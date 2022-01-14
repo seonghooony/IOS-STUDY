bfc2cf252c5aa7f01c69ad4b89e47a977f2322f5
seonghooony <sh-k@kakao.com>
2022-01-14 13:56:23 +0900

    22-1-13 study

---
+++ 04. upper intermediate/02. MyAssets/MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by SeongHoon Kim on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

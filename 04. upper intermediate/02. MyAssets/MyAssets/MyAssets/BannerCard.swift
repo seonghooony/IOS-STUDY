91a99b9cfc7cc515628350b52d5007cd99c603a2
seonghooony <sh-k@kakao.com>
2022-01-17 12:55:55 +0900

    22-1-17 study

---
+++ 04. upper intermediate/02. MyAssets/MyAssets/MyAssets/BannerCard.swift
@@ -0,0 +1,30 @@
+//
+//  BannerCard.swift
+//  MyAssets
+//
+//  Created by SeongHoon Kim on 2022/01/17.
+//
+
+import SwiftUI
+
+struct BannerCard: View {
+    var banner: AsssetBanner
+    var body: some View {
+        Color(banner.backgroundColor)
+            .overlay {
+                VStack {
+                    Text(banner.title)
+                        .font(.title)
+                    Text(banner.description)
+                        .font(.subheadline)
+                }
+            }
+    }
+}
+
+struct BannerCard_Previews: PreviewProvider {
+    static var previews: some View {
+        let banner0 = AsssetBanner(title: "공지사항", description: "추가된 공지사항을 확인하세요", backgroundColor: .red)
+        BannerCard(banner: banner0)
+    }
+}
---
+++ 04. upper intermediate/02. MyAssets/MyAssets/MyAssets/Entities/AssetBanner.swift
@@ -0,0 +1,14 @@
+//
+//  AssetBanner.swift
+//  MyAssets
+//
+//  Created by SeongHoon Kim on 2022/01/17.
+//
+
+import UIKit
+
+struct AsssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}

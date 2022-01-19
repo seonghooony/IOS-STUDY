56a84b275be6b31d007f399b3dc0952928406d92
seonghooony <sh-k@kakao.com>
2022-01-19 19:22:22 +0900

    22-1-19 study

---
+++ 04. upper intermediate/02. MyAssets/MyAssets/MyAssets/AssetSummaryData.swift
@@ -0,0 +1,20 @@
+//
+//  AssetSummaryData.swift
+//  MyAssets
+//
+//  Created by SeongHoon Kim on 2022/01/19.
+//
+
+import SwiftUI
+
+struct AssetSummaryData: ObservableObject {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AssetSummaryData_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetSummaryData()
+    }
+}
---
+++ 04. upper intermediate/02. MyAssets/MyAssets/MyAssets/Entities/Asset.swift
@@ -0,0 +1,34 @@
+//
+//  Asset.swift
+//  MyAssets
+//
+//  Created by SeongHoon Kim on 2022/01/19.
+//
+
+import Foundation
+
+class Asset: Identifiable, ObservableObject, Decodable {
+    let id: Int
+    let type: AssetMenu
+    let data: [AssetData]
+    
+    init(id: Int, type: AssetMenu, data: [AssetData]) {
+        self.id = id
+        self.type = type
+        self.data = data
+    }
+}
+
+class AssetData: Identifiable, ObservableObject, Decodable {
+    let id: Int
+    let title: String
+    let amount: String
+    
+    init(id: Int, title: String, amount: String) {
+        self.id = id
+        self.title = title
+        self.amount = amount
+    }
+}
+
+        

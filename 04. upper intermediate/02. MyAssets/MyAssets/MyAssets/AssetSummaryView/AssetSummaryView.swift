613678279db1758b53833434e3a5b141e393c609
seonghooony <sh-k@kakao.com>
2022-01-20 18:03:37 +0900

    22-1-20 study

---
+++ 04. upper intermediate/02. MyAssets/MyAssets/MyAssets/AssetSummaryView/AssetSummaryView.swift
@@ -0,0 +1,35 @@
+//
+//  AssetSummaryView.swift
+//  MyAssets
+//
+//  Created by SeongHoon Kim on 2022/01/20.
+//
+
+import SwiftUI
+
+struct AssetSummaryView: View {
+    @EnvironmentObject var assetData: AssetSummaryData
+    
+    var assets: [Asset] {
+        return assetData.assets
+    }
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            ForEach(assets) { asset in
+                AssetSectionView(assetSection: asset)
+            }
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding()
+        }
+    }
+}
+
+struct AssetSummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetSummaryView()
+            .environmentObject(AssetSummaryData())
+            .background(Color.gray.opacity(0.2))
+    }
+}
--- 04. upper intermediate/02. MyAssets/MyAssets/MyAssets/Entities/Asset.swift
+++ 04. upper intermediate/02. MyAssets/MyAssets/MyAssets/Entities/Asset.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 class Asset: Identifiable, ObservableObject, Decodable {
     let id: Int
@@ -23,12 +24,66 @@ class AssetData: Identifiable, ObservableObject, Decodable {
     let id: Int
     let title: String
     let amount: String
+    let creditCardAmount: [CreditCardAmounts]?
     
-    init(id: Int, title: String, amount: String) {
+    init(id: Int, title: String, amount: String, creditCardAmount: [CreditCardAmounts]? = nil) {
         self.id = id
         self.title = title
         self.amount = amount
+        self.creditCardAmount = creditCardAmount
     }
 }
 
+enum CreditCardAmounts: Identifiable, Decodable {
+    
+case previousMonth(amount: String)
+case currentMonth(amount: String)
+case nextMonth(amount: String)
+    
+    var id: Int {
+        switch self {
+        case .previousMonth:
+            return 0
+        case .currentMonth:
+            return 1
+        case .nextMonth:
+            return 2
+        }
+    }
+    
+    var amount: String {
+        switch self {
+        case .previousMonth(let amount),
+             .currentMonth(let amount),
+             .nextMonth(let amount):
+            return amount
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case previousMonth
+        case currentMonth
+        case nextMonth
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
         
+        if let value = try? values.decode(String.self, forKey: .previousMonth) {
+            self = .previousMonth(amount: value)
+            return
+        }
+        
+        if let value = try? values.decode(String.self, forKey: .currentMonth) {
+            self = .currentMonth(amount: value)
+            return
+        }
+        
+        if let value = try? values.decode(String.self, forKey: .nextMonth) {
+            self = .nextMonth(amount: value)
+            return
+        }
+        
+        throw fatalError("ERROR: CreditCardAomunts Json Decoding Error")
+    }
+}

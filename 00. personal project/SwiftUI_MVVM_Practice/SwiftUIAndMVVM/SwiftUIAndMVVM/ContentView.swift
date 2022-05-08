93e277d84b61886306093de6b86920d9aa2e5483
seonghooony <sh-k@kakao.com>
2022-05-08 21:28:38 +0900

    Study MVVM and SwiftUIs

---
+++ 00. personal project/SwiftUI_MVVM_Practice/SwiftUIAndMVVM/SwiftUIAndMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAndMVVM
+//
+//  Created by SeongHoon Kim on 2022/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00. personal project/SwiftUI_MVVM_Practice/SwiftUIAndMVVM/SwiftUIAndMVVM/SwiftUIAndMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAndMVVMApp.swift
+//  SwiftUIAndMVVM
+//
+//  Created by SeongHoon Kim on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAndMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
